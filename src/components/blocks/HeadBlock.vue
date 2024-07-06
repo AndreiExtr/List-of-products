dcd185fa142712c49c18bc7bf970c12dcc838cba
Андрей кропинов <andreikropinov17121988@gmail.com>
2024-07-06 22:44:19 +0300

    add head

--- src/components/blocks/HeadBlock.vue
+++ src/components/blocks/HeadBlock.vue
@@ -1,14 +1,25 @@
 <template>
   <header class="head">
     <a>НАША ПРОДУКЦИЯ</a>
+    <div class="head_shopping">
+      <div class="head_shopping_text">
+        <p>3 товара</p>
+        <p>на сумму 3 500 ₽</p>
+      </div>
+      <div class="head_shopping_bt">
+        <BasketIcon />
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
+import BasketIcon from '../icons/BasketIcon.vue'
 
 export default {
   name: 'HeadBlock',
   components: {
+    BasketIcon
   },
   props: {
   }
@@ -27,10 +38,36 @@ export default {
   background-color: #161516;
 
   a{
-    font-weight: 700;
+    font-weight: 500;
     font-size: 31px;
     margin: 0;
     color: #ffffff;
+  }
+
+  &_shopping{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 20px;
+
+    &_text{
+      font-weight: 500;
+      font-size: 17px;
+      text-align: left;
+      color: #ffffff;
+      text-align: right;
+    }
+
+    &_bt{
+      width: 40px;
+      height: 40px;
+      border-radius: 20px;
+      background-color: #D58C51;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+    }
   }
 }
 </style>
